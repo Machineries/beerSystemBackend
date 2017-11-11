e0e132ebf08defb0c6681bed2e16746ca2a4d26c
Mattias Linder <mattias.linder87@gmail.com>
2017-11-11 12:30:24 +0100

    get sortiment from API

--- server.js
+++ server.js
@@ -1,41 +1,67 @@
-var express = require('express'),
-app = express(),
+const https = require('https');
+var express = require('express');
+var jsonfile = require('jsonfile');
+var parser = require('fast-xml-parser');
+
+const sortimentFilePath = './json/sortiment.json';
+const lastFetchedFilePath = './json/lastFetched.json';
+
+app = express();
 port = process.env.PORT || 3000;
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
-
-app.get('/sortiment', function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');    
-    var data = require('./sortiment.json');
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify(
-        data
-    ));
-    res.end(''); // End the JSON array and response.);
+app.get('/sortiment', function(req, mainRes, next) {
+    mainRes.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');    
+    mainRes.writeHead(200, {'Content-Type': 'application/json'});
+    if (shouldFetch()) {
+        var path = 'https://www.systembolaget.se/api/assortment/products/xml';
+        var r = https.get(path, function(res) {
+            var xml = '';
+            // // Data comes in chunks
+            res.on('data', function(chunk) {
+                xml += chunk;
+            }).on('end', function() {
+                // Parse xml string to json
+                var json = parser.parse(xml);
+                // Save to filr
+                jsonfile.writeFile(sortimentFilePath, json, function (err) {
+                    console.error(err)
+                });
+                jsonfile.writeFile(lastFetchedFilePath, {date: new Date()}, function (err) {
+                    console.error(err)
+                });
+                var jsonString = JSON.stringify(json);
+                mainRes.write(jsonString);
+                mainRes.end('');
+            })
+        });
+        r.on('error', function(e) {
+            mainRes.write(getSortimentFromFile());
+            mainRes.end('');
+        });
+    } else {
+        mainRes.write(getSortimentFromFile());
+        mainRes.end('');
+    }
 });
 
-app.get('/image', function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');   
-    var Curl = require( 'node-libcurl' ).Curl;
-    var curl = new Curl();
-    curl.setOpt('URL', 'craftbeer.se/sites/craftbeer.live6.client.udev.se/files/styles/product_image/public/pictures/154715.png?itok=CtU-OaKZ' );
-    curl.setOpt('FOLLOWLOCATION', true);
-    curl.on('end', function( statusCode, body, headers ) {
-        console.info("got it");
-        
-        console.info( statusCode );
-        console.info( '---' );
-        console.info( body.length );
-        console.info( body );
-        res.write(body);
-        console.info( this.getInfo( 'TOTAL_TIME' ) );
-     
-    res.write('hello world');
-    res.end('');
-        this.close();
-    });
-    curl.on( 'error', curl.close.bind( curl ) );
-    curl.perform();
-});
+var getSortimentFromFile = function() {
+    var sortiment = jsonfile.readFileSync(sortimentFilePath);
+    if (sortiment) {
+        return JSON.stringify(sortiment);
+    } else {
+        return JSON.stringify({error: "can't read file: " + sortimentFilePath});
+    }
+}
+
+var shouldFetch = function() {
+    var file = './json/lastFetched.json';
+    var lastFetched = jsonfile.readFileSync(file);
+    if (lastFetched) {
+        var date = new Date(lastFetched.date);
+        var today = new Date();
+        return date.toDateString() !== today.toDateString();
+    }
+    return false;
+}
